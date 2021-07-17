f171e7daa26fc5aee96e7947fcf4f78247391330
k-koech <kelvin.koech@student.moringaschool.com>
2021-07-17 18:31:03 +0300

    removed quotes in an array

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -12,8 +12,6 @@ export class QuoteComponent{
   quotes:Quote[] = [
     new Quote(1,'The greatest glory in living lies not in never falling, but in rising every time we fall.','Nelson Mandela', 'Kelvin',new Date(2021,6,13),0,0),
     new Quote(2,'The way to get started is to quit talking and begin doing.','Walt Disney', 'Mercy',new Date(2017,1,24),0,0),
-    new Quote(3,'Don\'t judge each day by the harvest you reap but by the seeds that you plant.','Robert Louis Stevenson', 'Mike Mwangi',new Date(2020,11,31),0,0),
-    new Quote(4,'Whoever is happy will make others happy too.','Anne Frank', 'Joel',new Date(2021,1,9),0,0),
   ];
    
   
