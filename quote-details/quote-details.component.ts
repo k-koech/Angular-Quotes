bc5507b0b5ccc24e8f2428276150e82f6cd03a4e
k-koech <kelvin.koech@student.moringaschool.com>
2021-07-17 16:20:27 +0300

    updated quote-details component

---
+++ quote-details/quote-details.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+
+@Component({
+  selector: 'app-quote-details',
+  templateUrl: './quote-details.component.html',
+  styleUrls: ['./quote-details.component.css']
+})
+export class QuoteDetailsComponent implements OnInit {
+  @Input() quotesFromChild!: Quote;
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
